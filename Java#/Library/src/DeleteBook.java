3d4f96fe38f6b72553658b1f6913ebc1fa2f682a
necati <necati_akyazi@outlook.de>
2020-12-31 22:01:04 +0100

    bla

--- Java#/Library/src/DeleteBook.java
+++ Java#/Library/src/DeleteBook.java
@@ -1,8 +1,22 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class DeleteBook implements ActionListener {
 
@@ -19,13 +33,20 @@ public class DeleteBook implements ActionListener {
     public DeleteBook(){
 
         createGUI();
-
-        deleteBook();
+        button.addActionListener(this::actionPerformed);
 
     }
 
-    private void deleteBook() {
-
+    private void deleteBook(Document doc) {
+        Node root = doc.getDocumentElement();
+        NodeList books = doc.getElementsByTagName("book");
+        Element bookElement = null;
+        for (int i = 0; i < books.getLength(); i++) {
+            bookElement = (Element) books.item(i);
+            if (bookElement.getAttribute("id").equals(book.getText())){
+                root.removeChild(bookElement);
+            }
+        }
 
 
     }
@@ -71,6 +92,33 @@ public class DeleteBook implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse("books.xml");
 
+            deleteBook(doc);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+
+            StreamResult result = new StreamResult(new File("books.xml"));
+            transformer.setOutputProperty(OutputKeys.INDENT,"yes");
+            transformer.transform(source, result);
+
+            frame.dispose();
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (TransformerConfigurationException transformerConfigurationException) {
+            transformerConfigurationException.printStackTrace();
+        } catch (ParserConfigurationException parserConfigurationException) {
+            parserConfigurationException.printStackTrace();
+        } catch (SAXException saxException) {
+            saxException.printStackTrace();
+        } catch (TransformerException transformerException) {
+            transformerException.printStackTrace();
+        }
     }
 }
